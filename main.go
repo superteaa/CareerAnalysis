687e63c94ba907d0800625a7c5ed0c3ad412cafa
superteaa <1059175003@qq.com>
2024-09-01 17:16:34 +0800

    优化

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://8.138.115.27:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://8.138.115.27:8866"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
